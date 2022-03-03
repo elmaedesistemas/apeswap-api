f8845bda60c5f04b9ef45472d22a0ee6efc10287
Ixm 759 <759ixm@gmail.com>
2022-03-03 16:14:37 -0600

    chore: Nodes polygon

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -107,8 +107,8 @@ export default () => ({
       //'https://rpc-mainnet.matic.network',
       // 'https://matic-mainnet.chainstacklabs.com',
       // 'https://rpc-mainnet.maticvigil.com',
-      'https://rpc-mainnet.matic.quiknode.pro',
-      'https://matic-mainnet-full-rpc.bwarelabs.com',
+      // 'https://rpc-mainnet.matic.quiknode.pro',
+      // 'https://matic-mainnet-full-rpc.bwarelabs.com',
     ],
     abi: {
       masterApe: MASTER_APE_ABI_POLYGON,
